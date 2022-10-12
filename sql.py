1c6ca2c65bfdf88105d65215cb914506636b01a7
Nigel Murphy <murphynzl1@gmail.com>
2022-10-12 20:33:13 +1300

    new

---
+++ sql.py
@@ -0,0 +1,33 @@
+from secrets import choice
+import mysql.connector
+from mysql.connector import Error
+
+def con_db(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+connection = con_db("localhost","nigel","zaq12wsx","project")
+
+opt=input("What do want to do (Create, edit or alter)?")
+
+print(opt)
